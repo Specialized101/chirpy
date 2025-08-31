2bcd59b05c28689ffac3a8309099e0b7dcbbb5e9
Specialized101 <yassinefares17@gmail.com>
2025-08-31 17:29:33 +0200

    Add /healthz to check server status

--- main.go
+++ main.go
@@ -13,7 +13,13 @@ const (
 func main() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/", fs)
+	mux.Handle("/app/", http.StripPrefix("/app", fs))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		r.Header.Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 
 	server := &http.Server{
 		Addr:    ADDR + ":" + PORT,
